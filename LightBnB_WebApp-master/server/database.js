f7a4cd162082f5aa10d00dcce63cba0ccee3e019
Catherine Hu <catherinehu_9898@hotmail.com>
2021-07-22 19:21:25 +0000

    updated getUserWithId, getUserWithEmail, addUser

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -16,17 +16,23 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
-}
+  return pool
+    .query(`
+  SELECT *
+  FROM users
+  WHERE email = $1;`, [email])
+    .then(res => {
+      if (res.rows[0]) {
+        return res.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.err(err.message)
+    });
+};
+
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -35,8 +41,23 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
-}
+  return pool
+    .query(`
+  SELECT *
+  FROM users
+  WHERE id = $1;` [id])
+    .then(res => {
+      if (res.rows[0]) {
+        return res.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch(err => {
+      console.log(err.message)
+    });
+};
+
 exports.getUserWithId = getUserWithId;
 
 
@@ -46,11 +67,18 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+  return pool
+    .query(`
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;`, [user.name, user.email, user.password])
+    .then(newUser => {
+      return newUser;
+    })
+    .catch(err => {
+      console.log(err.message);
+    })
+};
 exports.addUser = addUser;
 
 /// Reservations
@@ -74,9 +102,9 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 
- const getAllProperties = (options, limit = 10) => {
+const getAllProperties = (options, limit = 10) => {
   return pool
-  .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
     .then((result) => {
       console.log(result.rows);
     })
